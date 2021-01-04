7f3c2a2902d5e2fb132922f89471919bcab4b562
Jose G. Juarez R <josejuarez@outlook.com>
2021-01-03 20:28:26 -0500

    Add project files.

---
+++ CollectionEquallity/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace CollectionEquallity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			DateTime[] bankHols1 =
+			{
+				new DateTime(2021, 1, 1),
+				new DateTime(2021, 4, 2),
+				new DateTime(2021, 4, 5),
+				new DateTime(2021, 5, 3),
+				new DateTime(2021, 5, 31),
+				new DateTime(2021, 8, 30),
+				new DateTime(2021, 12, 27),
+				new DateTime(2021, 12, 28),
+			};
+
+			DateTime[] bankHols2 =
+			{
+				new DateTime(2021, 1, 1),
+				new DateTime(2021, 4, 2),
+				new DateTime(2021, 4, 5),
+				new DateTime(2021, 5, 3),
+				new DateTime(2021, 5, 31),
+				new DateTime(2021, 8, 30),
+				new DateTime(2021, 12, 27),
+				new DateTime(2021, 12, 28),
+			};
+			DateTime[] bankHols3 = bankHols1;
+			// different references
+			Console.WriteLine($"bn1==bh2? {bankHols1 == bankHols2}");
+			// different references
+			Console.WriteLine($"bn2==bh3? {bankHols2 == bankHols3}");
+			// same references
+			Console.WriteLine($"bn3==bh1? {bankHols3 == bankHols1}");
+			Console.WriteLine($"Equal values? {bankHols1.SequenceEqual(bankHols2)}");
+			Console.ReadKey();
+		}
+    }
+}
